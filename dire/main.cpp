dad4300bc31cf985f4d96c25b8ed85e26df671af
Fly <67563726+FlyUltra@users.noreply.github.com>
2023-07-27 13:46:12 +0200

    Small update

--- dire/main.cpp
+++ dire/main.cpp
@@ -1,12 +1,29 @@
+#ifdef _WIN32
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#endif
+
 #include <iostream>
+#include <string>
+#include <ws2tcpip.h>
 
 int main() {
 
-    while(true) {
-
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize" << std::endl;
+        return 1;
     }
 
-    std::cout << "Server is listening on port 8081." << std::endl;
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Failed to create" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
